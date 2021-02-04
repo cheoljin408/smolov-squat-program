b00fddcf685ed5bb3a0e8e9793c07325def50c34
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-02-04 14:46:06 +0900

    Workout1 컴포넌트 추가

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Head from "./Head";
+import Workout1 from "./Workout1";
 
 function Workout() {
     return (
     <>
         <Head />
+        <Workout1 />
     </>
     );
 }
---
+++ src/components/Workout1.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const DayBlock = styled.div`
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    padding-top: 30px;
+    padding-bottom: 30px;
+`;
+
+const WorkoutBlock = styled.div`
+    width: 450px;
+    /* height: 80px; */
+    border: 1px solid #e9ecef;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    padding-left: 30px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+
+    .p1 {
+        width: 100px;
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+    .p2 {
+        margin-left: 30px;
+        width: 280px;
+        height: 20px;
+        margin-top: 8px;
+        margin-bottom: 8px;
+    }
+
+    .div1 {
+        display: flex;
+        align-items: center;
+    }
+`;
+
+function Workout1() {
+    return (
+        <DayBlock>
+            <WorkoutBlock>
+                <div>
+                    <p className="p1">Monday</p>
+                </div>
+                <div>
+                    <div className="div1">
+                        <p className="p2">3 sets x 8 reps @ 65%</p>
+                        <div>
+                            <input type="checkbox"/>
+                        </div>
+                    </div>
+                    <div className="div1">
+                        <p className="p2">1 sets x 5 reps @ 70%</p>
+                        <input type="checkbox"/>
+                    </div>
+                    <div className="div1">
+                        <p className="p2">2 sets x 2 reps @ 75%</p>
+                        <input type="checkbox"/>
+                    </div>
+                    <div className="div1">
+                        <p className="p2">1 sets x 1 reps @ 80%</p>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+            </WorkoutBlock>
+        </DayBlock>
+    );
+}
+
+export default Workout1;
